d7715fa2df4a4db6b477db56ebf9964c4a5ca028
juliocsantaman <juliocsantaman@gmail.com>
2020-06-02 12:40:39 -0500

    Get height of the window screen.

---
+++ js/screen.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoad', function() {
+  let height = window.screen.height;
+  let main = document.querySelector('main');
+  main.style.minHeight = height + "px";
+  console.log(height);
+});
